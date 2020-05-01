d0838459680e28beb09389718d764032a6d1c501
Aditya Raj <aditya.x510@gmail.com>
2020-05-01 08:54:50 +0530

    Initial commit

---
+++ Day-1/sample-one.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    int y = 7;
+    cout << "Value of x and  is : " << x << " and " << y;
+    cout << endl;
+    cout << "Sum is : " << x + y << " and Product is : " << x * y;
+    cout << endl;
+
+    return 0;
+}
---
+++ Day-1/usingNamespace.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!!" << endl;
+    cout << "Using namespace std";
+    return 0;
+}
